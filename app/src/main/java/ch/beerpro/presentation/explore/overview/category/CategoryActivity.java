908ce3f5d0e744c2a45d3a7f74944860dd04147e
lukas <->
2018-11-03 17:15:37 +0100

    Kategorie: 'Zurück' funktioniert

--- app/src/main/java/ch/beerpro/presentation/explore/overview/category/CategoryActivity.java
+++ app/src/main/java/ch/beerpro/presentation/explore/overview/category/CategoryActivity.java
@@ -10,10 +10,12 @@ import android.widget.ImageView;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 import ch.beerpro.R;
 import ch.beerpro.domain.models.Beer;
+import ch.beerpro.presentation.MainActivity;
 import ch.beerpro.presentation.details.DetailsActivity;
 import ch.beerpro.presentation.explore.overview.ViewPagerAdapter;
 import ch.beerpro.presentation.profile.mybeers.OnMyBeerItemInteractionListener;
@@ -63,5 +65,11 @@ public class CategoryActivity extends AppCompatActivity
     @Override
     public void onWishClickedListener(Beer item) {
         categoryViewModel.toggleItemInWishlist(item.getId());
+    }
+
+    @Override
+    public void onBackPressed()     {
+        Intent intent = NavUtils.getParentActivityIntent(this);
+        startActivity(intent);
     }
 }
